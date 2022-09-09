f6d24f394e128d4093737c6073b633cf1890b777
feras alnowiser <ferasdev@gmail.com>
2022-09-09 23:43:24 +0300

    adding the user-get page

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
     res.send('<h1>Tuwaiq Academy - JavaScript</h1> <br> <h2> MongoDB, Express, React, and Node JS</h2>');
 });
 
+app.get('/user-get', (req, res) => {
+    res.send('<h1>This is POST REQUEST</h1> <br> <h2>Welcome to GET REQUEST page</h2>');
+})
+
+
+
 // SERVER listen on port number 3000
 app.listen(port, () => {
     console.log('SERVER IS WORKING ...');
