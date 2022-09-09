35ee587a2312f4519bafb0203af4ba480928ed47
feras alnowiser <ferasdev@gmail.com>
2022-09-09 23:45:04 +0300

    adding the put request page

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// the home page
+// the home page ------------------------
 app.get('/', (req, res) => {
     res.send('<h1>Tuwaiq Academy - JavaScript</h1> <br> <h2> MongoDB, Express, React, and Node JS</h2>');
 });
 
+// GET Request ------------------------
 app.get('/user-get', (req, res) => {
     res.send('<h1>This is POST REQUEST</h1> <br> <h2>Welcome to GET REQUEST page</h2>');
 })
@@ -18,6 +19,11 @@ app.get('/user-get', (req, res) => {
 app.post('/user-post', (req, res) => {
     res.send('<h1>This is POST REQUEST</h1> <br> <h2>Welcome to POST REQUEST page</h2>');
 })
+
+// PUT Request ------------------------
+app.put('/user-put/:index', (req, res) => {
+    res.send('<h1>This is PUT REQUEST</h1> <br> <h2>Welcome to PUT REQUEST page</h2>');
+});
 
 // SERVER listen on port number 3000
 app.listen(port, () => {
