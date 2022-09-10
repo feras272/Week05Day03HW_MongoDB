644d33894cea9f39985f274493aa9f4a707a6f9e
feras alnowiser <ferasdev@gmail.com>
2022-09-11 01:26:49 +0300

    add: the db library to app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ console.log("Week 5 Day 3 - MongoDB");
 const express = require('express');
 const app = express();
 const port = 3000;
+const db = require('./db')
 
 // the home page ------------------------
 app.get('/', (req, res) => {
