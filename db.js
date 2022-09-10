05e4dedc02b99f28f75b67f4b5467ab02d176557
feras alnowiser <ferasdev@gmail.com>
2022-09-11 01:26:13 +0300

    connect to mongo database and testing for if it success or failed

--- db.js
+++ db.js
@@ -4,3 +4,17 @@ const mongoURI = 'mongodb://localhost:27017/test';
 
 const db = mongoose.connection;
 
+// Connect to Mongo
+mongoose.connection(mongoURI, { userNewUrlParser: true, useUnifiedTopology: true}, () => {
+    console.log('DB IS WORKING, CONNECTION ESTABLISHED ...')
+})
+
+// Extr: on error 
+db.on('error', err => {
+    console.log(err.message + 'mongo nor running')
+})
+
+// Extr: on success
+db.on('error', err => {
+    console.log('Mongo connected ...')
+})
