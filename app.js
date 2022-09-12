f2697909191a9d5cc96339eeb8b697c065efd4e1
feras alnowiser <ferasdev@gmail.com>
2022-09-12 14:27:43 +0300

    add: User create method to insert user object into database

--- app.js
+++ app.js
@@ -7,24 +7,42 @@ const express = require('express');
 const db = require('./db');
 const app = express();
 const port = 3000;
+const User = require('./models/user')
 
 // Reading the json inside the body of the request
 app.use(express.json());
+
+// const handleError = function(err) {
+//     console.error(err);
+//     // handle your error
+// };
 
 // the home page ------------------------
 app.get('/', (req, res) => {
     res.send('<h1>Tuwaiq Academy - JavaScript</h1> <br> <h2> MongoDB, Express, React, and Node JS</h2>');
 });
 
+// MongoDB 
+// console.log('=============================')
+// console.log(User)
+
 // GET Request ------------------------
-app.get('/user-get', (req, res) => {
+app.get('/all', (req, res) => {
+
     res.send('<h1>This is POST REQUEST</h1> <br> <h2>Welcome to GET REQUEST page</h2>');
+    
 })
 
 // POST Request ------------------------
-app.post('/user-post', (req, res) => {
-    res.send('<h1>This is POST REQUEST</h1> <br> <h2>Welcome to POST REQUEST page</h2>');
-})
+app.post('/insert', (req, res) => {
+    //res.send('<h1>This is POST REQUEST</h1> <br> <h2>Welcome to POST REQUEST page</h2>');
+    User.create({firstName:'Feras', favoriteFood:'Shawerma'}, (err, newData) => {
+        if (err) return handleError(err);
+
+        console.log('DATA: ', newData)
+    })
+   
+});
 
 // PUT Request ------------------------
 app.put('/user-put/:index', (req, res) => {
@@ -40,3 +58,16 @@ app.delete('/user-delete', (req, res) => {
 app.listen(port, () => {
     console.log('SERVER IS WORKING ...');
 })
+
+
+// function handleError(err) {
+//     console.error(err)
+// }
+
+    // { firstName: "Alnowiser", favoriteFood: "Shawerma" }, (err, newData) => {
+    // if(err) {
+    //     return handleError(err);
+    //     //console.log('Error Happened')
+    // }
+    // console.log('DATA: ' + newData); 
+// });
