0536712b8c0494a074f91a20f6b8297f38164059
feras alnowiser <ferasdev@gmail.com>
2022-09-12 13:57:48 +0300

    fix and organize the code in db.js

--- db.js
+++ db.js
@@ -7,14 +7,14 @@ const mongoURI = 'mongodb://localhost:27017/test';
 const db = mongoose.connection;
 
 // Connect to Mongo & take three parameters
-mongoose.connect(mongoURI, { userNewUrlParser: true, useUnifiedTopology: true}, () => {
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true}, () => {
     /*
     Parameters:
         1) take the Mongo link
         2) object options 
         3) callback when the connect established
     */
-    console.log('DB IS WORKING, CONNECTION ESTABLISHED ...')
+    console.log('1) DB IS WORKING, CONNECTION ESTABLISHED ...')
 })
 
 // Extr: on error 
@@ -25,5 +25,5 @@ db.on('error', err => {
 
 // Extr: on success
 db.on('error', err => {
-    console.log('Mongo connected ...')
+    console.log('Mongo CONNECTED ...')
 })
